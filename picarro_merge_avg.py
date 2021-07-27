7ce744f8c3d921d55dc50de127ee6a4365a2ac6a
Saurabh Annadate <saurabh.annadate@hotmail.com>
2021-07-27 17:37:00 +0530

    Update picarro_merge_avg.py

--- picarro_merge_avg.py
+++ picarro_merge_avg.py
@@ -4,27 +4,25 @@ import pandas as pd
 import glob
 import os
 
-path = '/media/vsinha/HP v301w/New folder/PICCARO/July/'
+path = '/home/PICARRO/July/'  #path to the folder consisting daywise folders of the PICARRO data
 arr = os.listdir(path)
-all_files = []
+all_files = []                # list of all the data files need to merged
 for i in arr:
 	all_files = all_files + glob.glob(path+i + "/*.dat")
 	
 	
-li = []
+li = []    # list for Picarro files as pandas dataframes
 
 for filename in all_files:
-    #frame = pd.read_csv(filename,sep='\t', encoding='cp1252',skiprows=9)  #ptrms
     frame = pd.read_csv(filename,sep="\s+")   #piccaro
     li.append(frame)
     
-frame = pd.concat(li, axis=0, ignore_index=True)
-#frame['datetime_ptrms'] = pd.to_datetime(frame['datetime_ptrms'])
-#frame = frame.sort_values(by = 'datetime_ptrms')
-frame['DATE_TIME'] = frame['DATE']+" "+ frame['TIME']
-frame['DATE_TIME'] =  pd.to_datetime(frame['DATE_TIME'])
-frame = frame.sort_values(by = 'DATE_TIME')
-#frame.to_csv('merged_8feb_14july_picarro.csv')
-piccaro_1min = frame.resample('1Min',on='DATE_TIME').mean()
-piccaro_1min.to_csv('picarro_1minavg_july.csv')
+frame = pd.concat(li, axis=0, ignore_index=True)  #concatanate all separate dataframes into one dataframe
+
+frame['DATE_TIME'] = frame['DATE']+" "+ frame['TIME']  #merge DATE and TIME columns to DATE_TIME
+frame['DATE_TIME'] =  pd.to_datetime(frame['DATE_TIME']) #read it as pandas datetime format
+frame = frame.sort_values(by = 'DATE_TIME')  #sort the dataframe by DATE_TIME
+#frame.to_csv('merged_picarro.csv')
+piccaro_1min = frame.resample('1Min',on='DATE_TIME').mean() #1 minute average file
+piccaro_1min.to_csv('picarro_1minavg_july.csv') #save as csv file
 
