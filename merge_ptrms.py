3ac1c7532d3c10d872f244d21fcf76de8ef68dc6
Saurabh Annadate <saurabh.annadate@hotmail.com>
2021-08-25 23:26:03 +0530

    Update merge_ptrms.py

--- merge_ptrms.py
+++ merge_ptrms.py
@@ -1,1 +1,8 @@
+#author: Saurabh Annadate
+import pandas as pd
 
+PTRMS = pd.read_csv('ptrms_data.csv',skiprows=9)  #load ptc or csv
+PTRMS['datetime_ptrms'] = pd.to_datetime(PTRMS['datetime_ptrms'],dayfirst=True)
+PTRMS = PTRMS.set_index('datetime_ptrms')
+PTRMS_4min = PTRMS.resample('4T').mean()
+PTRMS_4min.to_csv('ptrms_4minavg.csv')
