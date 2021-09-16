d4a3b748c7d875f4aa2b1ae40df6566e49dfcd43
Saurabh Annadate <saurabh.annadate@hotmail.com>
2021-09-16 12:51:34 +0530

    Update merge_ptrms.py

--- merge_ptrms.py
+++ merge_ptrms.py
@@ -1,8 +1,27 @@
 #author: Saurabh Annadate
 import pandas as pd
+import glob
+import os
 
-PTRMS = pd.read_csv('ptrms_data.csv',skiprows=9)  #load ptc or csv
-PTRMS['datetime_ptrms'] = pd.to_datetime(PTRMS['datetime_ptrms'],dayfirst=True)
-PTRMS = PTRMS.set_index('datetime_ptrms')
-PTRMS_4min = PTRMS.resample('4T').mean()
-PTRMS_4min.to_csv('ptrms_4minavg.csv')
+path = '/media/vsinha/YASH THAKUR/ptrms clean/New folder/'  #path to the folder consisting daywise folders of the PICARRO data
+arr = os.listdir(path)
+all_files = []                # list of all the data files need to merged
+for i in arr:
+	all_files = all_files + glob.glob(path+i + "/*.ptc")
+		
+li = []    # list for Picarro files as pandas dataframes
+
+#data = pd.read_csv(all_files[0],sep='\t',encoding= 'unicode_escape',skiprows= 9)
+#print(data)
+
+for filename in all_files:
+    frame = pd.read_csv(filename,sep='\t',encoding= 'unicode_escape',skiprows= 9)   #ptrms
+    li.append(frame)
+    
+frame = pd.concat(li, axis=0, ignore_index=True)  #concatanate all separate dataframes into one dataframe
+
+frame['datetime_ptrms'] =  pd.to_datetime(frame['datetime_ptrms']) #read it as pandas datetime format
+frame = frame.sort_values(by = 'datetime_ptrms')  #sort the dataframe by DATE_TIME
+#frame.to_csv('merged_picarro.csv')
+ptrms_4min = frame.resample('4Min',on='datetime_ptrms').mean() #4 minute average file
+ptrms_4min.to_csv('ptrms_ptc_Nov_Aug.csv') #save as csv file
